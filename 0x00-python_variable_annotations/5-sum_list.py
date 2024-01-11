ebcefb7aa570b45a8b56c951979128bc4c8d0c41
besufikad <07yilmabese@gmail.com>
2024-01-11 20:41:49 +0300

    t 5, 6 ,7 edited

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -10,7 +10,7 @@ def sum_list(input_list: List[float]) -> float:
     The function `sum_list` takes a list of floats as input
     and returns the sum of all the elements in the list.
     """
-    result = 0
-    for i in input_list:
-        result += i
-    return float(result)
+    if input_list is None:
+        return 0
+    else:
+        return float(sum(input_list))
