6381a37e5c876847d12164978bf102f73f1ce398
besufikad <07yilmabese@gmail.com>
2024-01-11 15:49:15 +0300

    t 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,11 @@
 """
 code starts here
 """
+
+
 def add(a: float, b: float) -> float:
+    """
+    The function "add" takes two float
+    numbers as input and returns their sum as a float.
+    """
     return a + b
