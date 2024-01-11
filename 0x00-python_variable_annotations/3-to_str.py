dcb628bc1d26ffe6351e775435657c2bac5482c6
besufikad <07yilmabese@gmail.com>
2024-01-11 16:01:05 +0300

    t 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+code starts here
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    The function `to_str` converts a float number to a string.
+    """
+    return str(n)
