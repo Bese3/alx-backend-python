84354b87d2faab9c16e135ccb3368b4f7a8bd8f7
besufikad <07yilmabese@gmail.com>
2024-01-11 19:39:56 +0300

    t 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import Tuple, Union
+"""
+code starts here
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    The function takes a key (k) and a value (v) and returns a tuple
+    with the key and the square of the value.
+    """
+    return (k, v**2)
