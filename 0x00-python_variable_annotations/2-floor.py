9af1d487fcfe48d220178b92a913ae25679d7677
besufikad <07yilmabese@gmail.com>
2024-01-11 15:57:30 +0300

    t 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+code starts here
+"""
+
+
+def floor(n: float) -> int:
+    """
+    The function `floor` takes a float as input
+    and returns the largest integer less than or equal to
+    the input.
+    """
+    return int(n)
