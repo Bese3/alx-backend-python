e8fdbcd0afa755b36cbfbd0e41423b317647def0
besufikad <07yilmabese@gmail.com>
2024-01-15 14:49:39 +0300

    edited 3 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
-'''
-The `task_wait_n` function takes in two parameters, `n` and `max_delay`,
-and returns a list of floats
-'''
+"""Contains a method that spawns Tasks n times with a
+specified delay between each call."""
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
